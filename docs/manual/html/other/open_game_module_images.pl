5b944d838428da6fed1b919ddbb364691f5cac8d
Jay Convertino <electrobs@gmail.com>
2025-05-29 18:46:17 -0400

    update manual with usage images

--- docs/manual/html/other/open_game_module_images.pl
+++ docs/manual/html/other/open_game_module_images.pl
@@ -7,10 +7,25 @@ $cached_env_img{$key} = q|<IMG
  STYLE="height: 0.23ex; vertical-align: -0.12ex; " SRC="|."$dir".q|open_game_module_img2.svg"
  ALT="$ $">|; 
 
-$key = q/includegraphics[width=0.50textwidth,keepaspectratio]{imgslashSPARKLETRON.png};AAT/;
+$key = q/includegraphics[width=0.58textwidth,keepaspectratio]{imgslashcompleted_ogm.jpg};AAT/;
 $cached_env_img{$key} = q|<IMG
- STYLE="height: 3.47ex; vertical-align: -0.12ex; " SRC="|."$dir".q|open_game_module_img5.svg"
- ALT="\includegraphics[width=0.50\textwidth,keepaspectratio]{img/SPARKLETRON.png}">|; 
+ STYLE="height: 46.53ex; vertical-align: -0.12ex; " SRC="|."$dir".q|open_game_module_img8.svg"
+ ALT="\includegraphics[width=0.58\textwidth,keepaspectratio]{img/completed_ogm.jpg}">|; 
+
+$key = q/includegraphics[width=0.58textwidth,keepaspectratio]{imgslashget_ogm.jpg};AAT/;
+$cached_env_img{$key} = q|<IMG
+ STYLE="height: 46.53ex; vertical-align: -0.12ex; " SRC="|."$dir".q|open_game_module_img5.svg"
+ ALT="\includegraphics[width=0.58\textwidth,keepaspectratio]{img/get_ogm.jpg}">|; 
+
+$key = q/includegraphics[width=0.58textwidth,keepaspectratio]{imgslashinsert_ogm.jpg};AAT/;
+$cached_env_img{$key} = q|<IMG
+ STYLE="height: 46.53ex; vertical-align: -0.12ex; " SRC="|."$dir".q|open_game_module_img7.svg"
+ ALT="\includegraphics[width=0.58\textwidth,keepaspectratio]{img/insert_ogm.jpg}">|; 
+
+$key = q/includegraphics[width=0.58textwidth,keepaspectratio]{imgslashopen_exp.jpg};AAT/;
+$cached_env_img{$key} = q|<IMG
+ STYLE="height: 46.53ex; vertical-align: -0.12ex; " SRC="|."$dir".q|open_game_module_img6.svg"
+ ALT="\includegraphics[width=0.58\textwidth,keepaspectratio]{img/open_exp.jpg}">|; 
 
 $key = q/includegraphics[width=0.90textwidth,height=textheight,keepaspectratio]{imgslashSPARKLETRON.png};AAT/;
 $cached_env_img{$key} = q|<IMG
